261356801501392b6678559fce083e9f2dd5bed2
mohamadsajedi <mohamadsajedi75@ggmail.com>
2019-09-28 14:23:40 +0330

    second commit

--- web/models.py
+++ web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Expense(models.Model):
+    desc = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=True)
+    def __str__(self):
+        return "{}-{}".format(self.desc, self.date)
+
+class Income(models.Model):
+    desc = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=True)
+    def __str__(self):
+        return "{}-{}".format(self.desc, self.date)
+
+

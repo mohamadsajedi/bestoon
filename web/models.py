4857773034275c6e5ce2d16e232409ab3b1a09bd
mohamadsajedi <mohamadsajedi75@ggmail.com>
2019-10-01 15:46:56 +0330

    upload on server1

--- web/models.py
+++ web/models.py
@@ -26,3 +26,9 @@ class Token(models.Model):
         return "{}-{}".format(self.now, self.user)
 
 
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)  # TODO: do not save password
--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('submit/expense/', views.submit_expense, name='submit_expense')
+    path('submit/expense/', views.submit_expense, name='submit_expense'),
+    path('register/', views.register, name='register'),
+    path('', views.index, name='index'),
+    path('logout/', views.logout, name='logout')
 ]
